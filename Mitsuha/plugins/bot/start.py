e24e560a03527f1a007f37356c547d4c519dfb0e
RiyoGod <godsxrio@gmail.com>
2025-02-14 11:35:22 +0530

    Update start.py

--- Mitsuha/plugins/bot/start.py
+++ Mitsuha/plugins/bot/start.py
@@ -1,40 +1,51 @@
-import time
+import asyncio
 import random
+import time
 from pyrogram import filters
 from pyrogram.enums import ChatType
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 import config
 from Mitsuha import app
 from Mitsuha.misc import _boot_
 from Mitsuha.plugins.sudo.sudoers import sudoers_list
-from Mitsuha.utils.database import get_served_chats, get_served_users, get_sudoers
 from Mitsuha.utils import bot_sys_stats
 from Mitsuha.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
     get_lang,
+    get_served_chats,
+    get_served_users,
     is_banned_user,
     is_on_off,
 )
 from Mitsuha.utils.decorators.language import LanguageStart
 from Mitsuha.utils.formatters import get_readable_time
-from Mitsuha.utils.inline import help_pannel, private_panel, start_panel
-from config import BANNED_USERS
+from Mitsuha.utils.inline.start import private_panel, start_panel
+from Mitsuha.utils.inline.help import first_page
+from config import BANNED_USERS, MITSUHA, START_IMG_URL
 from strings import get_string
 
-#--------------------------
-
-NEXI_VID = [
-"https://envs.sh/CeR.me.mp4",
-"https://envs.sh/TV1.me.mp4",
-"https://envs.sh/Ce3.me.mp4",
+ANNIE_VID = [
+    "https://files.catbox.moe/7boa9v.mp4",
+    "https://files.catbox.moe/mbd0ik.mp4",
+    "https://files.catbox.moe/ol5agj.mp4",
+    "https://files.catbox.moe/hhtzc0.mp4",
 ]
 
+STICKERS = [
+    "CAACAgQAAyEFAASTre2gAAImrGd_cLg7LjCyySRi79Ww_MxN-vNqAAIhCgACBK-4U3-aoVNMnVdFHgQ",
+    "CAACAgIAAxkBAAICPGeCahvYHfTYIb_PHPVAQuYHf8YCAAJ0WgACJKiASgkR6SGIY_xqHgQ",
+    "CAACAgQAAyEFAASTre2gAAImsmd_cR68JMrQLtlqYT9lHYY_p-A1AALACgAC0km4Uw993Zg0chHBHgQ",
+    "CAACAgIAAxkBAAICPmeCakIdXfmBSJpTFKQSMHi2zNkeAAKVVgACS8AxSvtwBWEFdvaCHgQ",
 
+]
+
+async def delete_sticker_after_delay(message, delay):
+    await asyncio.sleep(delay)
+    await message.delete()
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
@@ -43,23 +54,22 @@ async def start_pm(client, message: Message, _):
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
-            keyboard = help_pannel(_)
-            return await message.reply_video(
-                random.choice(NEXI_VID),
+            keyboard = first_page(_)
+            await message.reply_photo(
+                photo=START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
-        if name[0:3] == "sud":
+        elif name[0:3] == "sud":
             await sudoers_list(client=client, message=message, _=_)
             if await is_on_off(2):
-                return await app.send_message(
+                await app.send_message(
                     chat_id=config.LOGGER_ID,
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
-            return
-        if name[0:3] == "inf":
+        elif name[0:3] == "inf":
             m = await message.reply_text("🔎")
-            query = (str(name)).replace("info_", "", 1)
+            query = str(name).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
             for result in (await results.next())["result"]:
@@ -83,30 +93,36 @@ async def start_pm(client, message: Message, _):
                 ]
             )
             await m.delete()
-            await app.send_photo(
+            await app.send_video(
                 chat_id=message.chat.id,
-                photo=thumbnail,
+                video=thumbnail,
                 caption=searched_text,
                 reply_markup=key,
             )
             if await is_on_off(2):
-                return await app.send_message(
+                await app.send_message(
                     chat_id=config.LOGGER_ID,
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
     else:
         out = private_panel(_)
+        sticker_message = await message.reply_sticker(sticker=random.choice(STICKERS))
+        asyncio.create_task(delete_sticker_after_delay(sticker_message, 2))
+        served_chats = len(await get_served_chats())
+        served_users = len(await get_served_users())
+        UP, CPU, RAM, DISK = await bot_sys_stats()
         await message.reply_video(
-            random.choice(NEXI_VID),
-            caption=_["start_2"].format(message.from_user.mention, app.mention),
+            random.choice(ANNIE_VID),
+            caption=random.choice(AYUV).format(
+                message.from_user.mention, app.mention, UP, DISK, CPU, RAM, served_users, served_chats
+            ),
             reply_markup=InlineKeyboardMarkup(out),
         )
         if await is_on_off(2):
-            return await app.send_message(
+            await app.send_message(
                 chat_id=config.LOGGER_ID,
                 text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
             )
-
 
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
@@ -114,12 +130,11 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_video(
-        random.choice(NEXI_VID),
+        random.choice(ANNIE_VID),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
     return await add_served_chat(message.chat.id)
-
 
 @app.on_message(filters.new_chat_members, group=-1)
 async def welcome(client, message: Message):
@@ -149,7 +164,7 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_video(
-                    random.choice(NEXI_VID),
+                    random.choice(ANNIE_VID),
                     caption=_["start_3"].format(
                         message.from_user.mention,
                         app.mention,
